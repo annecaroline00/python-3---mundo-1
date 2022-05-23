144210f74a9bd00bc0946dd985cbb00efe5d2965
annecaroline00 <annecaroline@ufpi.edu.br>
2022-05-23 18:01:54 -0300

    commit master

---
+++ tratando dados e fazendo contas/oparimeticas.py
@@ -0,0 +1,9 @@
+# como fazer operações ariméticas
+    # 5/2 == 2.5
+    # // --> divisão inteira (5//2 == 2) dividir sem usar vírgula 
+    # % --> resto da divisão (5%2 == 1) 
+# ordem de precedência:
+    # ()
+    # **
+    # * / // %
+    # + - 
