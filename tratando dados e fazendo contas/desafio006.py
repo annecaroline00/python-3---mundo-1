1f6ec103fa8d11333284c91bc0be00040aada40a
annecaroline00 <annecaroline@ufpi.edu.br>
2022-05-24 17:11:02 -0300

    commit master

--- tratando dados e fazendo contas/desafio006.py
+++ tratando dados e fazendo contas/desafio006.py
@@ -1,7 +1,7 @@
 n1 = int(input('Digite um número:'))
 
-n2 = n1**2
-n3 = n1**3
-n4 = n1**1/2
+n2 = n1*2
+n3 = n1*3
+n4 = n1**(1/2)
 
 print('O número digitado foi {}, o seu dobro é {}, o seu triplo é {}, sua raiz quadrada é {} :)'.format(n1,n2,n3,n4))
